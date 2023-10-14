3cf92af3b47788e3e8f34cf94c8b11f726978538
Andrey <86359412+Santas7@users.noreply.github.com>
2023-10-14 16:04:55 +0300

    Update task3.asm

--- task3.asm
+++ task3.asm
@@ -1,1 +1,22 @@
+%include "io64_float.inc"
 
+section .rodata
+    two dd 2.0
+    a dd 1.0
+    b dd 3.0
+
+section .data
+    x dd 0.0
+
+section .text
+    global main
+    main:
+        fld dword[two]     
+        fld dword[b]   ; ST(1) = b
+        f2xm1
+        fpatan            
+        fld dword[a]      
+        fsub              ; arctan(2^b) - a
+        fstp dword[x]     
+        PRINT_FLOAT x 
+        ret
