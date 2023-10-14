87b83c81bafb7a9a544b15190c2e49257dde16f4
Andrey <86359412+Santas7@users.noreply.github.com>
2023-10-14 16:08:04 +0300

    Update task3.asm

--- task3.asm
+++ task3.asm
@@ -2,21 +2,26 @@
 
 section .rodata
     two dd 2.0
-    a dd 1.0
-    b dd 3.0
 
+section .bss
+    a resq 1
+    b resq 1
+    
 section .data
     x dd 0.0
 
 section .text
     global main
     main:
+        READ_FLOAT a
+        READ_FLOAT b
         fld dword[two]     
-        fld dword[b]   ; ST(1) = b
+        fld dword[b]
         f2xm1
         fpatan            
         fld dword[a]      
         fsub              ; arctan(2^b) - a
+        fabs
         fstp dword[x]     
         PRINT_FLOAT x 
         ret
