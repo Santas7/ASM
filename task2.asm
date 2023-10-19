1d074ea371d1231cb050ec01ad3b04d18acce00e
Andrey <86359412+Santas7@users.noreply.github.com>
2023-10-19 10:31:59 +0300

    Update task2.asm

--- task2.asm
+++ task2.asm
@@ -12,25 +12,27 @@ section .text
         READ_FLOAT xmm0
     
         ; приближение cos(x) = a0 - (x^2/2!) + (x^4/4!) - (x^6/6!) + ... 
+        ; 1 часть
         movss xmm1, xmm0
         mulss xmm1, xmm0  ; x^2
-        
         movss xmm2, xmm1
         movss xmm3, [a0]
         ; x^2/2!
         divss xmm2, [factorial2]
         subss xmm3, xmm2  ; a0 - x^2/2!
         
+        ; 2 часть
         movss xmm2, xmm1
         movss xmm4, [factorial4]
         ; x^4/4!
         divss xmm2, xmm4
         addss xmm3, xmm2  ; a0 - x^2/2! + x^4/4!
         
-        ; x^6/6!
+        ; 3 часть
         movss xmm2, xmm1
         mulss xmm2, xmm1  ; x^6
         movss xmm4, [factorial6]
+        ; x^6/6!
         divss xmm2, xmm4
         subss xmm3, xmm2  ; a0 - x^2/2! + x^4/4! - x^6/6! + ...
         
