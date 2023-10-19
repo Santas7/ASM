b2aa43dcf204663026710a165a1d10e9a7308433
Andrey <86359412+Santas7@users.noreply.github.com>
2023-10-19 12:01:47 +0300

    Update task4.asm

--- task4.asm
+++ task4.asm
@@ -3,8 +3,7 @@
 section .rodata
     a dd 0.5
     exp dd 2.7182818284
-    one dd 1.0
-    two dd 2.0
+    two dd 2.0 ; просто число 2
     
 section .bss
     x resq 1
@@ -16,61 +15,57 @@ section .bss
 section .text
     global main
     main:
+        call checker
+        ret
+        
+    checker:
         READ_FLOAT x
         READ_FLOAT y
-
+        ; Вычисление 2 * a/x
         fld dword[a]     
         fld dword[x]     
-        fdiv
-        fstp dword[x]
-        fld dword[x]  
+        fdiv 
         fld dword[two]
         fmul  
         fstp dword[x]
         
-        ; Вычислить e^(2(a/x)) - 1
-        fld dword[x]     
-        fild dword[exp]  
-        movss xmm0, dword[x]
-        movss dword[numerator], xmm0
-        fld dword[numerator]
-        fldl2e
+        ; Вычисление e^(2(a/x)) + 1   
+        fld dword[exp]  
+        fld dword[x]
+        fldl2e ; log2[e]
         fmulp st1, st0
         fld st0
         frndint
         fsub st1, st0
         fxch st1
         f2xm1
-        fld1
+        fld1 ; const 1.0
         faddp st1, st0
         fscale
-        fld dword[one]
-        fadd 
+        fld st1
+        fadd ; + 1
+        fstp dword[denominator]
+
+        ; Вычисление e^(2(a/x)) - 1
+        fld dword[exp]  
+        fld dword[x]
+        fldl2e ; log2[e]
+        fmulp st1, st0
+        fld st0
+        frndint
+        fsub st1, st0
+        fxch st1
+        f2xm1
+        fld1 ; const 1.0
+        faddp st1, st0
+        fscale
+        fld st1 
+        fsub ; - 1
         fstp dword[numerator]
 
-        ; Вычислить e^(2(a/x)) + 1
-        fld dword[x]
-        fld dword[exp]
-        movss xmm0, dword[x]
-        movss dword[denominator], xmm0
-        fld dword[denominator]
-        fldl2e
-        fmulp st1, st0
-        fld st0
-        frndint
-        fsub st1, st0
-        fxch st1
-        f2xm1
-        fld1
-        faddp st1, st0
-        fscale
-        fld dword[one]
-        fsub 
-        fstp dword[denominator]
-        
-        ; частное числитель/знаменатель
-        fld dword[numerator] 
+        ; частное числитель/знаменатель - th(a/x)
         fld dword[denominator] 
+        fld dword[numerator] 
         fdiv
         fstp dword[right] 
         PRINT_STRING "Left: "
@@ -81,18 +76,13 @@ section .text
         NEWLINE
         
         ; проверка на равенство левой и правой части
-        movss xmm0, dword[right]
-        movss xmm1, dword[y]
-        cmpeqss xmm0, xmm1
-        je answer_yes
-        jmp answer_no
-       
-    answer_yes:
-        PRINT_STRING "YES equal"
-        jmp end
-    
-    answer_no:
-        PRINT_STRING "NO equal"
-    
-    end:
+        fld dword[right] 
+        fld dword[y] 
+        comip
+        jb no
+        PRINT_STRING "YES" 
+        ret
+
+    no:
+        PRINT_STRING "NO"
         ret
