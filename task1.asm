3525d311977178c885fa4c5b52850bffedc9ff64
Andrey <86359412+Santas7@users.noreply.github.com>
2023-11-09 13:49:56 +0400

    Add files via upload

---
+++ task1.asm
@@ -0,0 +1,78 @@
+section .rodata
+    two dd 2.0
+    x db "", 0dh, 0
+    a db "%f", 0
+    b db "%f", 0
+
+section .text
+    global main
+    extern printf, scanf, malloc, free
+
+main:
+    push rbp
+    mov rbp, rsp 
+    sub rsp, 40
+    mov rcx, 8
+    call malloc
+    mov r12, rax
+    push rbp
+    mov rbp, rsp 
+    sub rsp, 40
+    lea rcx, [b]
+    mov rdx, r12
+    call scanf
+    fld dword [b]
+    leave
+    push rbp
+    mov rbp, rsp 
+    sub rsp, 40
+    mov rcx, r12
+    call free
+    leave
+    fld dword [two]   
+    fyl2x
+    fld1
+    fld st1
+    fprem
+    f2xm1
+    fadd
+    fscale
+    fld st1
+    fpatan
+    push rbp
+    mov rbp, rsp 
+    sub rsp, 40
+    mov rcx, 8
+    call malloc
+    mov r12, rax
+    push rbp
+    mov rbp, rsp 
+    sub rsp, 40
+    lea rcx, [a]
+    mov rdx, r12
+    call scanf
+    fld dword [a]
+    leave
+    push rbp
+    mov rbp, rsp 
+    sub rsp, 40
+    mov rcx, r12
+    call free
+    leave     
+    fsub
+    fabs
+    fstp dword [x]
+    push rbp
+    mov rbp, rsp 
+    sub rsp, 40
+    lea rcx, [x]
+    mov rdx, [r12]
+    call printf
+    leave
+    push rbp
+    mov rbp, rsp 
+    sub rsp, 40
+    mov rcx, r12
+    call free
+    leave
+    ret
