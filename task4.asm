826bbb6a70ede2666686412cea94bc2c82ffe89f
Andrey <86359412+Santas7@users.noreply.github.com>
2023-11-09 11:48:46 +0400

    Update task4.asm

--- task4.asm
+++ task4.asm
@@ -1,16 +1,14 @@
 %include "io64_float.inc"
 
 section .rodata
-    a dd 0.5
-    exp dd 2.7182818284
-    two dd 2.0 ; просто число 2
-    
-section .bss
-    x resq 1
-    y resq 1
-    numerator resq 1 ; числитель
-    denominator resq 1 ; знаменатель
-    right resq 1  ; правая часть выражения
+    a: dd 0.5
+    exp: dd 2.7182818284
+    two: dd 2.0 ; просто число 2
+    x: dd 0.0
+    y: dd 0.0
+    numerator: dd 0.0 ; числитель
+    denominator: dd 0.0; знаменатель
+    right: dd 0.0  ; правая часть выражения
 
 section .text
     global main
@@ -35,13 +33,13 @@ section .text
         fldl2e ; log2[e]
         fmulp st1, st0
         fld st0
-        frndint
-        fsub st1, st0
-        fxch st1
-        f2xm1
+        frndint ; округление к ближайшему целому
+        fsub st1, st0 
+        fxch st1 ; swap()
+        f2xm1 ; возводим в дробную часть 
         fld1 ; const 1.0
         faddp st1, st0
-        fscale
+        fscale ; возводим в целую часть + умножение
         fld st1
         fadd ; + 1
         fstp dword[denominator]
@@ -77,12 +75,12 @@ section .text
         
         ; проверка на равенство левой и правой части
         fld dword[right] 
-        fld dword[y] 
-        comip
-        jb no
-        PRINT_STRING "YES" 
+        fld dword[y]
+        fcomip ; условная инструкция
+        je yes
+        PRINT_STRING "NO" 
         ret
 
-    no:
-        PRINT_STRING "NO"
+    yes:
+        PRINT_STRING "YES"
         ret
