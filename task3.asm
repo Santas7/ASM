f6a577c9a3e33e18e05ba8dbcd5450f756b27a4e
Andrey <86359412+Santas7@users.noreply.github.com>
2023-11-09 11:47:58 +0400

    Update task3.asm

--- task3.asm
+++ task3.asm
@@ -1,24 +1,29 @@
 %include "io64_float.inc"
 
 section .rodata
-    two dd 2.0
-
-section .bss
-    a resq 1
-    b resq 1
+    two: dd 2.0
+    a: dd 0.0
+    b: dd 0.0
     
 section .data
-    x dd 0.0
+    x: dd 0.0
 
 section .text
     global main
     main:
         READ_FLOAT a
-        READ_FLOAT b
-        fld dword[two]     
+        READ_FLOAT b 
         fld dword[b]
-        f2xm1
-        fpatan            
+        fld dword[two]   
+        fyl2x ; вычисляем показатель
+        fld1 ; загружаем +1.0 в стек
+        fld st1 ;  дублируем показатель в стек
+        fprem ;  получаем дробную часть
+        f2xm1 ; возводим в дробную часть показателя
+        fadd ; прибавляем 1 из стека
+        fscale ; возводим в целую 
+        fld st1
+        fpatan ; arctan(2^b)          
         fld dword[a]      
         fsub              ; arctan(2^b) - a
         fabs
