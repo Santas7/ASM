5c3c1407eaa4c8e08059172cb0be42dfa96b4140
Andrey <86359412+Santas7@users.noreply.github.com>
2023-11-16 23:19:55 +0400

    Update task1.asm

--- task1.asm
+++ task1.asm
@@ -1,78 +1,73 @@
 section .rodata
     two: dd 2.0
-    x: db "", 0dh, 0
-    a: db "%f", 0
-    b: db "%f", 0
+    format: db "%f", 0
+
 
 section .text
     global main
-    extern printf, scanf, malloc, free
+    extern printf
+    extern scanf
+    extern malloc
+    extern free
 
-main:
-    push rbp
-    mov rbp, rsp 
-    sub rsp, 40
-    mov rcx, 8
-    call malloc
-    mov r12, rax
-    push rbp
-    mov rbp, rsp 
-    sub rsp, 40
-    lea rcx, [b]
-    mov rdx, r12
-    call scanf
-    fld dword [b]
-    leave
-    push rbp
-    mov rbp, rsp 
-    sub rsp, 40
-    mov rcx, r12
-    call free
-    leave
-    fld dword [two]   
-    fyl2x
-    fld1
-    fld st1
-    fprem
-    f2xm1
-    fadd
-    fscale
-    fld st1
-    fpatan
-    push rbp
-    mov rbp, rsp 
-    sub rsp, 40
-    mov rcx, 8
-    call malloc
-    mov r12, rax
-    push rbp
-    mov rbp, rsp 
-    sub rsp, 40
-    lea rcx, [a]
-    mov rdx, r12
-    call scanf
-    fld dword [a]
-    leave
-    push rbp
-    mov rbp, rsp 
-    sub rsp, 40
-    mov rcx, r12
-    call free
-    leave     
-    fsub
-    fabs
-    fstp dword [x]
-    push rbp
-    mov rbp, rsp 
-    sub rsp, 40
-    lea rcx, [x]
-    mov rdx, [r12]
-    call printf
-    leave
-    push rbp
-    mov rbp, rsp 
-    sub rsp, 40
-    mov rcx, r12
-    call free
-    leave
-    ret
+    main:
+        push r12 ;Сохранение неизменяемого регистра
+        push rbp
+        mov rbp, rsp 
+        sub rsp, 40
+        
+        ;malloc - выделение памяти на переменную, в которую считаем значение (её адрес в r12)
+        mov rcx, 8
+        call malloc
+        mov r12, rax
+        
+        ;Вызов scanf для float
+        lea rcx, [format]
+        mov rdx, r12
+        call scanf
+    
+        fld dword[r12]
+        
+        mov rcx, r12
+        call free
+        xor rax, rax
+        
+        mov rcx, 8
+        call malloc
+        mov r12, rax
+        
+        lea rcx, [format]
+        mov rdx, r12
+        call scanf
+        
+    
+        fld dword [two]   
+        fyl2x ; вычисляем показатель
+        fld1 ; загружаем +1.0 в стек
+        fld st1 ;  дублируем показатель в стек
+        fprem ;  получаем дробную часть
+        f2xm1 ; возводим в дробную часть показателя
+        fadd ; прибавляем 1 из стека
+        fscale ; возводим в целую 
+        fld st1
+        fpatan ; arctan(2^b) 
+        fld dword[r12]               
+        fsub              ; arctan(2^b) - a
+        fabs 
+        fstp dword[r12]
+        ;printf с параметром для float
+        lea rcx, [format]
+        movsd xmm1, qword[r12]
+        cvtps2pd xmm1, xmm1
+        movq rdx, xmm1
+        call printf
+        
+        ;free - очистка памяти в r12
+        mov rcx, r12
+        call free
+        xor rax, rax
+        
+        leave
+        pop r12 ;Восстановление неизменяемого регистра
+        xor rax, rax
+        ret
