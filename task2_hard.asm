a7e1923ad0c83edeba0f6d90fd716a637c067844
Andrey <86359412+Santas7@users.noreply.github.com>
2023-12-01 18:56:37 +0400

    Update task2_hard.asm

--- task2_hard.asm
+++ task2_hard.asm
@@ -1,1 +1,19 @@
+section .data
+    array: dq 1, 1
+    
+section .text
+    extern _ZN4hard4var61C6accessERNS0_1SE
+    global main
 
+main:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32
+    lea rcx, [array]
+    ; т к rcx - массив (по факту сравнение указателей)
+    mov [rdx], rcx
+    call _ZN4hard4var61C6accessERNS0_1SE
+    mov rsp, rbp
+    pop rbp
+    xor rax, rax
+    ret
